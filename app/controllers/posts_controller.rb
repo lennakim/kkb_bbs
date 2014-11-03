c62fd59c39fbbbac0c8ab89da61636c8326d7202
poppinyunhai <yiranyunhaisrh@gmail.com>
2014-11-04 00:41:54 +0800

    add user_id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all.page(params[:page]).per(params[:per_num])
+    if params[:node_id]
+      @node = Node.where(id: params[:node_id])
+      @posts = @node.posts.order("created_at DESC").page(params[:page]).per(24)
+    elsif params[:level]
+      @posts = Post.where(level: params[:level]).order("created_at DESC").page(params[:page]).per(24)
+    else
+      @posts = Post.all.order("level DESC", "created_at DESC").page(params[:page]).per(24)
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
     
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103162748) do
+ActiveRecord::Schema.define(version: 20141103164114) do
 
   create_table "nodes", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141103162748) do
     t.datetime "updated_at"
     t.integer  "level"
     t.integer  "node_id"
+    t.integer  "user_id"
   end
 
 end
