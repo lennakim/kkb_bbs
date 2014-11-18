b5a8ecbeff4a6c7b35bb6aa31e2f3e2c04c8b789
shooter <shooter.hero@gmail.com>
2014-11-18 15:23:40 +0800

    sso settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,43 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :login?, :current_user
+
   if Rails.env.production?
     http_basic_authenticate_with name: "kkb_bbs", password: "secret"
   end
+
+  def current_user
+    @current_user ||= login_from_session
+    @current_user
+  end
+
+  def login?
+    !!current_user
+  end
+
+  def login_as(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def login_from_session
+    if session[:user_id].present?
+      begin
+        User.find session[:user_id]
+      rescue
+        session[:user_id] = nil
+      end
+    end
+  end
+
+  def store_location(path)
+    session[:return_to] = path
+  end
+
 end
