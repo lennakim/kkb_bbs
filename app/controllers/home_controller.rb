e979f0c8c2b2bbd96d8fbc573b7839a5d42f3428
shooter <shooter.hero@gmail.com>
2014-11-20 21:39:48 +0800

    add paginate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 
   def welcome
     @nodes = Node.list
-    @topics = Topic.order("level DESC, created_at DESC").limit(10)
+    @topics = Topic.order("level DESC, created_at DESC").page(params[:page])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_datetime(datetime, format = "%Y-%m-%d %H:%M:%S")
+    datetime.strftime(format)
+  end
 end
