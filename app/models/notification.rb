79cf724ff8e109847db82743472eb9fcbe450450
shooter <shooter.hero@gmail.com>
2014-11-22 02:56:27 +0800

    dependent

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ActiveRecord::Base
 
+  belongs_to :user
+  belongs_to :subjectable, polymorphic: true
+  scope :unread, -> { where(read: false) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :topics
-  has_many :comments
+  has_many :topics, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :delete_all
 
   def self.login_user(login)
     User.where('lower(email) = ? or lower(name) = ?', login, login).first
