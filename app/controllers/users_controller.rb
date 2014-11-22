321937643ac8fc12519686ed671f387722e7b57d
shooter <shooter.hero@gmail.com>
2014-11-22 23:48:13 +0800

    user center

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @topics = @user.topics.recent.page(params[:page])
+    @comments = @user.comments.recent.page(params[:page])
+    @notifications = @user.notifications.recent.page(params[:page])
   end
 
   def me
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def avatar_tag path
+    avatar = path.blank? ? "head-img.gif" : path
+    image_tag avatar
+  end
 end
