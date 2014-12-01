b822687fc24b7b71c461d257a55d00e6cb7416b9
shooter <shooter.hero@gmail.com>
2014-12-01 20:14:40 +0800

    add
    zou qian wen

--- app/models/node.rb
+++ app/models/node.rb
@@ -4,4 +4,11 @@ class Node < ActiveRecord::Base
 
   scope :list, -> {order(created_at: :asc)}
   scope :recent, -> {order(created_at: :desc)}
+
+  def self.resource_node
+    if node = Node.find_by(title: '资源分享')
+      node.topics.recent.limit(5)
+    end
+  end
+
 end
