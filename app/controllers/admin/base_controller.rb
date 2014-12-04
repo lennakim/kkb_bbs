5cc512db5fdf1aee4179eb55c6bd55dbd9e3817d
shooter <shooter.hero@gmail.com>
2014-12-04 20:59:06 +0800

    up

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,6 +4,6 @@ class Admin::BaseController < ApplicationController
   before_action :auth_admin!
 
   def auth_admin!
-    redirect_to root_url, alert: t('message.not_authorized') unless (current_user && (current_user.s_admin? or current_user.admin?))
+    redirect_to root_url unless (current_user && (current_user.s_admin? or current_user.admin?))
   end
 end
